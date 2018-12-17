50cd51c4e35446f75ba93e2180bc49260485d198
loganocean <43765068+loganocean@users.noreply.github.com>
2018-12-17 11:16:04 -0500

    Add files via upload

---
+++ lesson9/problem2.py
@@ -0,0 +1,11 @@
+print('-'*70)
+print('Locker Bot:')
+print()
+password = input('What is the password? ')
+if password == 'Open Please':
+	print('Password is correct!!!')
+	print('Locker opening!!!')
+else:
+	print('Password invalid...enabling the lasers')
+	print('Pew Pew Pew')
+print('-'*70)
---
+++ lesson9/problem3.py
@@ -0,0 +1,9 @@
+print('-'*70)
+print('Alien Greeter Bot:')
+print()
+password = input('What planet are you from? ')
+if password == 'Mars' or password == "Venus" or password == 'Saturn':
+	print('Welcome to Earth you little green peanut head.')
+else:
+	print('OHHHHH I THOUGHT YOU WERE AN ALIEN, YOU UGLY!!!')
+print('-'*70)
